49807267527bf6abc3353d32f48494b71af6488b
Yordan Yordanov <yory_man@yahoo.com>
2019-04-05 13:32:25 +0300

    done
    last commit

--- src/compnents/Postform.js
+++ src/compnents/Postform.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { PropTypes } from "prop-types";
+import { createPost } from "../actions/postAction";
 
 class Postform extends Component {
   constructor(props) {
@@ -23,16 +26,7 @@ class Postform extends Component {
       title: this.state.title,
       body: this.state.body
     };
-
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json"
-      },
-      body: JSON.stringify(post)
-    })
-      .then(res => res.json())
-      .then(data => console.log(data));
+    this.props.createPost(post);
   };
 
   render() {
@@ -66,4 +60,11 @@ class Postform extends Component {
   }
 }
 
-export default Postform;
+Postform.propTypes = {
+  createPost: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { createPost }
+)(Postform);
